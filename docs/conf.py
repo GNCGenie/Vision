6213d822fc685c86664a931821fc544c36dc3fc3
R Narwar <rnarwar@inspecity.com>
2024-02-21 16:07:12 +0530

    docsHell

--- docs/conf.py
+++ docs/conf.py
@@ -4,8 +4,6 @@ import sys
 sys.path.insert(0, os.path.abspath('..'))
 sys.path.insert(0, os.path.abspath('../source'))
 
-import sphinx_rtd_theme
-
 # Project information
 project = 'Vision'
 copyright = '2023, Inspecity Space Labs'
@@ -27,21 +25,4 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
 # HTML output options
 html_theme = 'sphinx_rtd_theme'
-html_static_path = ['_static', '_figures']  # Remove unnecessary commas
-
-#html_theme_options = {
-#    'analytics_id': 'G-XXXXXXXXXX',  # Provided by Google in your dashboard
-#    'analytics_anonymize_ip': False,
-#    'logo_only': False,
-#    'display_version': True,
-#    'prev_next_buttons_location': 'bottom',
-#    'style_external_links': False,
-#    'vcs_pageview_mode': '',
-#    'style_nav_header_background': 'white',
-#    # Toc options
-#    'collapse_navigation': True,
-#    'sticky_navigation': True,
-#    'navigation_depth': 4,
-#    'includehidden': True,
-#    'titles_only': False
-#}
+html_static_path = ['_figures']  # Remove unnecessary commas
