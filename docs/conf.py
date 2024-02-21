a2813a33dd17eef27e4560913a6e8955bc1ef83d
R Narwar <rnarwar@inspecity.com>
2024-02-21 15:59:03 +0530

    stillbrokendocs

--- docs/conf.py
+++ docs/conf.py
@@ -1,13 +1,12 @@
 import os
 import sys
+
 sys.path.insert(0, os.path.abspath('..'))
 sys.path.insert(0, os.path.abspath('../source'))
 
 import sphinx_rtd_theme
 
-html_theme = "sphinx_rtd_theme"
-# -- Project information -----------------------------------------------------
-
+# Project information
 project = 'Vision'
 copyright = '2023, Inspecity Space Labs'
 author = 'R Narwar'
@@ -15,38 +14,23 @@ author = 'R Narwar'
 # The full version, including alpha/beta/rc tags
 release = '0.1'
 
-# -- General configuration ---------------------------------------------------
+# General configuration
+extensions = [
+    "sphinx_rtd_theme",
+    "sphinx.ext.autodoc",
+    "sphinx.ext.coverage",
+    "sphinx.ext.napoleon"
+]
 
-# Add any Sphinx extension module names here, as strings. They can be
-# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
-# ones.
-extensions = ["sphinx_rtd_theme",
-              "sphinx.ext.autodoc",
-              "sphinx.ext.coverage",
-              "sphinx.ext.napoleon"]
-
-# Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
-
-# List of patterns, relative to source directory, that match files and
-# directories to ignore when looking for source files.
-# This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
-# -- Options for HTML output -------------------------------------------------
-
-# The theme to use for HTML and HTML Help pages.  See the documentation for
-# a list of builtin themes.
-#
+# HTML output options
 html_theme = 'sphinx_rtd_theme'
-
-# Add any paths that contain custom static files (such as style sheets) here,
-# relative to this directory. They are copied after the builtin static files,
-# so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static','_figures']
+html_static_path = ['_static', '_figures']  # Remove unnecessary commas
 
 html_theme_options = {
-    'analytics_id': 'G-XXXXXXXXXX',  #  Provided by Google in your dashboard
+    'analytics_id': 'G-XXXXXXXXXX',  # Provided by Google in your dashboard
     'analytics_anonymize_ip': False,
     'logo_only': False,
     'display_version': True,
@@ -62,11 +46,6 @@ html_theme_options = {
     'titles_only': False
 }
 
-# Get the direct path
-dir_path = os.path.dirname(os.path.realpath(__file__))
-
-# Add the HTML logo that displays on the top-left panel.
-#html_logo = dir_path + '/_static/leogps_favicon2.png'
-
-# Add the HTML logo.
-#html_favicon = '_static/leogps_favicon.png'
+# Logo and favicon paths (update correctly)
+# html_logo = dir_path + '/_static/leogps_favicon2.png'
+# html_favicon = '_static/leogps_favicon.png'
