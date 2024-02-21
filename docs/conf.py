21daab72df4193069b3c895a674bfb503ec06ee9
R Narwar <rnarwar@inspecity.com>
2024-02-21 16:05:25 +0530

    update

--- docs/conf.py
+++ docs/conf.py
@@ -29,23 +29,19 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static', '_figures']  # Remove unnecessary commas
 
-html_theme_options = {
-    'analytics_id': 'G-XXXXXXXXXX',  # Provided by Google in your dashboard
-    'analytics_anonymize_ip': False,
-    'logo_only': False,
-    'display_version': True,
-    'prev_next_buttons_location': 'bottom',
-    'style_external_links': False,
-    'vcs_pageview_mode': '',
-    'style_nav_header_background': 'white',
-    # Toc options
-    'collapse_navigation': True,
-    'sticky_navigation': True,
-    'navigation_depth': 4,
-    'includehidden': True,
-    'titles_only': False
-}
-
-# Logo and favicon paths (update correctly)
-# html_logo = dir_path + '/_static/leogps_favicon2.png'
-# html_favicon = '_static/leogps_favicon.png'
+#html_theme_options = {
+#    'analytics_id': 'G-XXXXXXXXXX',  # Provided by Google in your dashboard
+#    'analytics_anonymize_ip': False,
+#    'logo_only': False,
+#    'display_version': True,
+#    'prev_next_buttons_location': 'bottom',
+#    'style_external_links': False,
+#    'vcs_pageview_mode': '',
+#    'style_nav_header_background': 'white',
+#    # Toc options
+#    'collapse_navigation': True,
+#    'sticky_navigation': True,
+#    'navigation_depth': 4,
+#    'includehidden': True,
+#    'titles_only': False
+#}
