fec887642b2a4e91f637871daba0f5866aea740b
R Narwar <rnarwar@inspecity.com>
2024-03-29 14:11:41 +0530

    update

--- src/video_stream_processing/vid_stream.py
+++ src/video_stream_processing/vid_stream.py
@@ -15,31 +15,26 @@ def load_cameras(n_cameras):
         cam.set(cv2.CAP_PROP_EXPOSURE , 1e0)
         cam.set(cv2.CAP_PROP_FRAME_WIDTH, 2592)
         cam.set(cv2.CAP_PROP_FRAME_HEIGHT, 1944)
-#        cam.set(cv2.CAP_PROP_FPS, 30)
         video_captures.append(cam)
         if len(video_captures) == n_cameras:
             break
     return video_captures
 
-def get_image(video_captures, release=False):
+def get_image(video_captures):
     disp=[]
     for i in range(n_cameras):
         _, image = video_captures[i].read()
-        if release:
-            video_captures[i].release()
         image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
         image = cv2.resize(image, (512, 384))
         disp.append(image)
     return disp
 
 import concurrent.futures
-def get_image_parallel(video_captures, release=False):
+def get_image_parallel(video_captures):
     n_cameras = len(video_captures)
     disp=[]
     def process_image(i):
         _, image = video_captures[i].read()
-        if release:
-            video_captures[i].release()
         image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
         image = cv2.resize(image, (512, 384))
         return image
@@ -55,10 +50,7 @@ release = False
 while True:
     # Measure time taken to read frame
     time_start = time.time()
-    if release:
-        video_captures = load_cameras(n_cameras)
     disp = get_image_parallel(video_captures, release)
-#    disp = get_image(video_captures, release)
     time_end = time.time()
     print('Time taken to read frame:', time_end - time_start, 'seconds')
 
@@ -68,13 +60,3 @@ while True:
     cv2.imshow('Display', disp)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         exit()
-
-    # Carry out auto exposure and contrast
-#    image = cv2.equalizeHist(image)
-
-    #_, image = cam.read()
-    ## Print details about the captured image
-    #print('Backend being used for capture:', cam.getBackendName())
-    #print('Image size:', image.shape)
-    #print('Image type:', image.dtype)
-    #print('Image channels:', image.ndim)
